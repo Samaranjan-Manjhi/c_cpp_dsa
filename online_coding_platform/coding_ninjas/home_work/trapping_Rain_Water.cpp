f7372cb0089fbaa22ccfa0569b01f3e7f52aa88f
root <root@samar.samar>
2026-07-02 00:23:22 +0530

    30/06/2026 H

--- online_coding_platform/coding_ninjas/home_work/trapping_Rain_Water.cpp
+++ online_coding_platform/coding_ninjas/home_work/trapping_Rain_Water.cpp
@@ -1,11 +1,11 @@
 /*
 
-Trapping Rain Water 
+   Trapping Rain Water 
 
- Problem statement
-You have been given a long type array/list 'arr’ of size 'n’.
-It represents an elevation map wherein 'arr[i]’ denotes the elevation of the 'ith' bar.
-Print the total amount of rainwater that can be trapped in these elevations.
+   Problem statement
+   You have been given a long type array/list 'arr’ of size 'n’.
+   It represents an elevation map wherein 'arr[i]’ denotes the elevation of the 'ith' bar.
+   Print the total amount of rainwater that can be trapped in these elevations.
 
 Note :
 The width of each bar is the same and is equal to 1.
@@ -26,10 +26,10 @@ Sample Output 1:
 2
 Explanation Of Sample Input 1:
 Water trapped by:
-     block of height 2 is 0 units.
-     block of height 1 is 1 unit.
-     block of height 1 is 3 1 unit. 
-     block of height 4 is 3 0 units.
+block of height 2 is 0 units.
+block of height 1 is 1 unit.
+block of height 1 is 3 1 unit. 
+block of height 4 is 3 0 units.
 Hence the total is 2.
 
 Sample Input 2:
@@ -39,11 +39,11 @@ Sample Output 2:
 9
 Explanation Of Sample Input 2:
 Water trapped by:
-     block of height 8 is 0 units.
-     block of height 1 is 7 units.
-     block of height 8 is 0 units. 
-     block of height 2 is 2 units.
-     block of height 4 is 0 units.
+block of height 8 is 0 units.
+block of height 1 is 7 units.
+block of height 8 is 0 units. 
+block of height 2 is 2 units.
+block of height 4 is 0 units.
 Hence the total is 9.
 
 Expected Time Complexity:
@@ -54,7 +54,7 @@ Constraints :
 0 <= arr[i] <= 10^9
 Time Limit: 1 sec
 
-*/
+ */
 
 #include <iostream>
 #include <vector>
@@ -63,34 +63,52 @@ using namespace std;
 
 long long getTrappedWater(vector<long long>& arr, int n) 
 {
-    // Write your code here.
-     int maxi = arr[n-1], l = 0, r = 0;
-     long long total = 0;
-     for(int i=n-2;i>=0;i--)
+     // Write your code here.
+     int l = 0, r = n-1;
+     int leftMax = 0, rightMax = 0;
+     long long water = 0;
+     while(l < r)
      {
-          r = n-1;
-          if(arr[i] > maxi)
+          if(arr[l] < arr[r])
           {
-               l = i;
-               int temp = min(maxi, arr[i]);
-               total = total + temp*(r-l);
-               for
+               if(arr[l] >= leftMax)
+               {
+                    leftMax = arr[l];
+               }
+               else
+               {
+                    water = water + (leftMax - arr[l]);
+               }
+               l++;
+          }
+          else
+          {
+               if(arr[r] >= rightMax)
+               {
+                    rightMax = arr[r];
+               }
+               else
+               {
+                    water = water + (rightMax - arr[r]);
+               }
+               r--;
           }
      }
+     return water;
 }
 
 int main()
 {
-int n;
-cout << "Enter Vector Size: ";
-cin >> n;
+     int n;
+     cout << "Enter Vector Size: ";
+     cin >> n;
 
-vector<long long> v(n);
-for(int i=0;i<n;i++)
-     cin >> v[i];
+     vector<long long> v(n);
+     for(int i=0;i<n;i++)
+          cin >> v[i];
 
-long long res = getTrappedWater(v, n);
-cout << "    Result:- " << res << endl;
+     long long res = getTrappedWater(v, n);
+     cout << "    Result:- " << res << endl;
 
-return 0;
+     return 0;
 }
